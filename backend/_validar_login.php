ce104747beda6c506859e754d02bdd08eb9f13a2
guidionisio5 <101677648+guidionisio5@users.noreply.github.com>
2022-08-17 20:02:30 -0300

    login finalizado

--- backend/_validar_login.php
+++ backend/_validar_login.php
@@ -7,7 +7,7 @@ try{
     $usuario = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM tb_login WHERE email='$usuario' AND senha = '$senha'";
+    $sql = "SELECT * FROM tb_login WHERE email='$usuario' AND senha = '$senha' AND ativo = 1";
 
     $comando = $con->prepare($sql);
 
@@ -15,7 +15,17 @@ try{
 
     $dados = $comando->fetchAll(PDO::FETCH_ASSOC);
 
-    var_dump($dados);
+    // var_dump($dados);
+
+    // verifica se existem resgistos dentro da variavel dados
+    if($dados != null){
+        // se o ususario e senha são validos, irá entrar nesse bloco de código
+        header('location: ../admin/gerenciar_viagens.php');
+    }else{
+        // se o usuario ou senha sÂo invalidos, iram entrar nesse bloco de código
+        // echo "Email ou senha inválida";
+        header('location: ../admin/index.html');
+    }
 
 }catch(PDOException $erro){
     echo $erro->getMessage();
